7d27e5697e14e894b165bb53c2bc2bf2edaf975b
Abhishek Shrivastav <89612684+abhi7862015@users.noreply.github.com>
2021-09-11 16:05:35 +0530

    Add files via upload

---
+++ Single page website/script.js
@@ -0,0 +1,56 @@
+// Zippy life section starts 
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    // var captionText = document.getElementById("caption");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    // captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+// Zippy life section End
+
+
+
+// Smooth Scrolling starts
+document.querySelectorAll('a[href="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+// Smooth Scrolling End
+
+
+
+
+// video projects starts 
+function videoslider1(links) {
+    document.querySelector(".slider").src = links;
+}
+// video projects End
+
+
+
